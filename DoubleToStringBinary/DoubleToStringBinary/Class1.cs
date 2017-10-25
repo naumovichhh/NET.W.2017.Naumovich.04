3ca3ff19445cdaf00387de46cfbcad65dc4a85d3
naumovichhh <32872842+naumovichhh@users.noreply.github.com>
2017-10-25 16:52:51 +0300

    Add files via upload

---
+++ DoubleToStringBinary/DoubleToStringBinary/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace Day4
+{
+    public static class DoubleToBinary
+    {
+        public static string ToBinary(this double source)
+        {
+            Union union = new Union();
+            union.Double = source;
+            return union.Long.ToBinary();
+        }
+
+        private static string ToBinary(this long source)
+        {
+            StringBuilder builder = new StringBuilder();
+            for (int i = 1; i <= 64; ++i)
+            {
+                if ((source & 1) == 0)
+                    builder.Insert(0, 0);
+                else
+                    builder.Insert(0, 1);
+                source >>= 1;
+            }
+            return builder.ToString();
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        private struct Union
+        {
+            public long Long
+            {
+                get { return m_long; }
+                set { m_long = value; }
+            }
+
+            public double Double
+            {
+                get { return m_double; }
+                set { m_double = value; }
+            }
+
+            [FieldOffset(0)] private long m_long;
+            [FieldOffset(0)] private double m_double;
+        }
+    }
+}
---
+++ DoubleToStringBinary/DoubleToStringBinaryTests/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Day4.Tests
+{
+    public static class DoubleToBinaryTests
+    {
+        [TestCase(255.255, "0100000001101111111010000010100011110101110000101000111101011100")]
+        [TestCase(-255.255, "1100000001101111111010000010100011110101110000101000111101011100")]
+        [TestCase(4294967295.0, "0100000111101111111111111111111111111111111000000000000000000000")]
+        [TestCase(double.MinValue, "1111111111101111111111111111111111111111111111111111111111111111")]
+        [TestCase(double.MaxValue, "0111111111101111111111111111111111111111111111111111111111111111")]
+        [TestCase(double.NaN, "1111111111111000000000000000000000000000000000000000000000000000")]
+        public static void ToBinaryTest(double source, string result)
+        {
+            Assert.AreEqual(source.ToBinary(), result);
+        }
+    }
+}
